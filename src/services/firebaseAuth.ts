3d1044078377a8642970b42e6adcee50736c3253
Joaquin Saldarriaga <jsaldatam@outlook.com>
2025-05-21 19:09:17 -0600

    feat: Enhance Firebase services with user registration and product addition functionality

--- src/services/firebaseAuth.ts
+++ src/services/firebaseAuth.ts
@@ -1,9 +1,16 @@
 // This Services file is part of the Firebase service integration
 // It contains the Firebase configuration and initializes the Firebase authentication service
 
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "@/services/firebase";
 
+
+// Register a new user
+export async function register(email: string, password: string) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Create a function to handle user login
 export async function login(email: string, password: string) {
   try {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
@@ -13,4 +20,9 @@ export async function login(email: string, password: string) {
     // handle error
     throw error;
   }
+}
+
+// Logout
+export async function logout() {
+  return await signOut(auth);
 }
--- src/services/firebaseProducts.ts
+++ src/services/firebaseProducts.ts
@@ -1,7 +1,7 @@
 // This Service file handles the Firebase product-related operations
 // such as fetching, adding, updating, & deleting products!
 
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 import { app } from "@/services/firebase";
 
 const db = getFirestore(app);
@@ -9,4 +9,10 @@ const db = getFirestore(app);
 export async function getProducts() {
   const querySnapshot = await getDocs(collection(db, "products"));
   return querySnapshot.docs.map(doc => doc.data());
+}
+
+// Function to add a new product
+export async function addProduct(product: any) {
+  return await addDoc(collection(db, "products"), product);
+
 }
